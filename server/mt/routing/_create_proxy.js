10cbe543bb6d5eaa37b70344b7cf964205e753f7
mattibf <mattibf@users.noreply.github.com>
2021-06-23 08:30:27 +0200

    Update _create_proxy.js

--- server/mt/routing/_create_proxy.js
+++ server/mt/routing/_create_proxy.js
@@ -39,9 +39,9 @@ module.exports = (server, method, route) => {
     case '/api/saved_objects/_bulk_get':
       options = require('./routes/bulk_get')(server, method, path);
       break;
-    case '/api/saved_objects/_export':
+    /* case '/api/saved_objects/_export':
       options = require('./routes/export')(server, method, path);
-      break;
+      break; */
     default:
       if (/\/api.*\/saved_objects\/_find/.test(route)) {
         options = require('./routes/find')(server, method, path);
